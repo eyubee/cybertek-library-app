b7afb89dfafff44f0c0f35f6cd8b841425172d60
eyubee <teferaey@gmail.com>
2021-08-04 02:27:54 -0400

    lib part2

--- src/test/java/com/cybertek/library/runners/CukesRunner.java
+++ src/test/java/com/cybertek/library/runners/CukesRunner.java
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
          glue = "com/cybertek/library/step_definitions",
         dryRun = false,
-      tags = " @test"
+      tags = ""
         )
 
 public class CukesRunner {
